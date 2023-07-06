cf328bec7542e0af4b402c940ee96e1711ef8fd8
T.E <125872197+Qu-tobechukwu@users.noreply.github.com>
2023-07-06 15:34:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,15 +8,11 @@ terraform {
 }
 
 provider "google" {
-  credentials = var.gcp-creds
+  credentials = file('mbk.json')
 
   project = "My-First-Project"
   region  = "us-west"
   zone    = "us-west4-b"
-}
-
-variable "gcp-creds"{
-default = "AIzaSyA0dlPEPdNbr0cF9rhiENIxwUfSfw71BhA"
 }
 
 resource "google_compute_network" "vpc_network" {

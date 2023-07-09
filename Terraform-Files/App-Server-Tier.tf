9467094794fb13fbd6b122bed8cf671a4ff616d9
T.E <125872197+Qu-tobechukwu@users.noreply.github.com>
2023-07-09 14:26:47 +0200

    Create App-Server-Tier.tf

---
+++ Terraform-Files/App-Server-Tier.tf
@@ -0,0 +1,4 @@
+# Create Private subnet (appserver level)
+# Create private route-table and connect it to app-server
+# Create e2-micro, ubuntu instances on each server
+# Connect Nat-Gateway to appservers

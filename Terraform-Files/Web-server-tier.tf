352fd718671dd8fd7dd959564979f2af786cedce
T.E <125872197+Qu-tobechukwu@users.noreply.github.com>
2023-07-09 14:30:27 +0200

    Update Web-server-tier.tf

--- Terraform-Files/Web-server-tier.tf
+++ Terraform-Files/Web-server-tier.tf
@@ -2,7 +2,7 @@
 # Create Nat-Gateway in Public subnet (webserver)
 # Create public route-table and connect it to web-server
 # Create e2-micro, ubuntu instances on each server
-
+# Create Firewall rules to open respective ports (HTTPS, HTTP)
 
 
 

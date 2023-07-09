97838f1669f500b246efbc397e30534575699b57
T.E <125872197+Qu-tobechukwu@users.noreply.github.com>
2023-07-09 15:31:15 +0200

    Update Web-server-tier.tf

--- Terraform-Files/Web-server-tier.tf
+++ Terraform-Files/Web-server-tier.tf
@@ -1,4 +1,14 @@
 # Create Public subnet (webserver level)
+resource "google_compute_subnetwork" "Web-Server-Layer-1" {
+  name          = "Web-Server-Layer-1"
+  #ip_cidr_range = "10.2.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
 # Create Nat-Gateway in Public subnet (webserver)
 # Create public route-table and connect it to web-server
 # Create e2-micro, ubuntu instances on each server

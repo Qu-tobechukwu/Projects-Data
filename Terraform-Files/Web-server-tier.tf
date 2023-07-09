eb56536dbbe0a05e024e3d8b5bc38ee5302abbe6
T.E <125872197+Qu-tobechukwu@users.noreply.github.com>
2023-07-09 14:27:05 +0200

    Update and rename Set-up-Servers.tf to Web-server-tier.tf

---
+++ Terraform-Files/Web-server-tier.tf
@@ -0,0 +1,9 @@
+# Create Public subnet (webserver level)
+# Create Nat-Gateway in Public subnet (webserver)
+# Create public route-table and connect it to web-server
+# Create e2-micro, ubuntu instances on each server
+
+
+
+
+

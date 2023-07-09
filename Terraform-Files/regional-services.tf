6704d4d6fad4fcf7e58723722a7f137e0f6e3eb2
T.E <125872197+Qu-tobechukwu@users.noreply.github.com>
2023-07-09 15:37:26 +0200

    Update regional-services.tf

--- Terraform-Files/regional-services.tf
+++ Terraform-Files/regional-services.tf
@@ -2,6 +2,13 @@
 # Create IGW
 # Create External & Internal 
 # Create bucket named: webapp-tf
+resource "google_storage_bucket" "auto-expire" {
+  name          = "webapp-tf"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
 
 
 
